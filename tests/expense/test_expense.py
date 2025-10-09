2e39ad2fbae8e317b70016340149f4b0470f843a
device <github@mjgoebel.de>
2025-10-09 21:30:21 +0200

    Fix expense_date initialization in test_add_expense_to_period_increases_total

--- tests/expense/test_expense.py
+++ tests/expense/test_expense.py
@@ -15,7 +15,7 @@ def test_init_period_with_balance():
 
 def test_add_expense_to_period_increases_total():
     start = datetime(year=2025, month=1, day=1, tzinfo=tz_berlin)
-    expense_date = datetime(2025,2, 1).date()
+    expense_date = datetime(2025,2, 1)
     period = Period(start=start, end=start + timedelta(days=365), initial_balance=100.10)
     expense = Expense(expenseid="expense-uuid", title="My Expense", notes="No notes for this expense", amount=100.00, category="Furniture", expense_date=expense_date)
 
